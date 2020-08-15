b6b9d10b28be8ccb90780eecac91d0ec790f6668
kubocker <kubocker@gmail.com>
2020-08-15 20:01:17 +0900

    Update url

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ func main() {
 	debug := true
 	w := webview.New(debug)
 	defer w.Destroy()
-	w.SetTitle("Minimal webview example")
+	w.SetTitle("kbk")
 	w.SetSize(600, 800, webview.HintNone)
-	w.Navigate("https://www.yahoo.co.jp")
+	w.Navigate("http://localhost:8100/")
 	w.Run()
 }
