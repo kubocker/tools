9e3766d887a46821af7e6f26da38a3712fc29ee5
kubocker <kubocker@gmail.com>
2020-08-23 16:49:26 +0900

    Develop millions page

--- app/src/Tabs.tsx
+++ app/src/Tabs.tsx
@@ -17,6 +17,7 @@ import { home, search, cog } from 'ionicons/icons';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Settings from './pages/Settings';
+import MillionsPage  from './pages/Millions';
 
 interface ItemProps extends RouteComponentProps<{ tab: string }> {
 }
@@ -25,19 +26,31 @@ const Tabs = ({ history, match }: ItemProps) => {
   return (
     <IonTabs>
       <IonRouterOutlet>
+        {/* 日々 / Daily */}
         <Route path="/app/home" component={Home} exact={true} />
+
+        {/* 学習 / Study */}
         <Route path="/app/search" component={Search} exact={true} />
+
+        {/* 資産 / Millions */}
+        <Route path="/app/millions" component={MillionsPage} />
+
+        {/* 設定 / Settings */}
         <Route path="/app/settings" component={Settings} />
         <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
         <IonTabButton tab="home" href="/app/home">
-          <IonIcon icon={home} />>
-          <IonLabel>Home</IonLabel>
+          <IonIcon icon={home} />
+          <IonLabel>ホームホーム</IonLabel>
         </IonTabButton>
         <IonTabButton tab="search" href="/app/search">
           <IonIcon icon={search} />
           <IonLabel>Search</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="millions" href="/app/millions">
+          <IonIcon icon={cog} />
+          <IonLabel>宝くじ</IonLabel>
         </IonTabButton>
         <IonTabButton tab="settings" href="/app/settings">
           <IonIcon icon={cog} />
--- app/src/pages/Millions.tsx
+++ app/src/pages/Millions.tsx
@@ -10,8 +10,12 @@ import {
   IonTitle,
   IonToolbar,
   IonItem,
-  IonList
+  IonList,
+  IonLabel,
 } from '@ionic/react';
+
+/* capacitor */
+import { Plugins } from '@capacitor/core';
 
 /* app */
 import { AppContext } from '../State';
@@ -19,6 +23,25 @@ import { authGuard } from '../Routing';
 
 /* others */
 import { History } from 'history';
+
+
+/**
+ * Capacitor
+ * プラグイン
+ */
+const {
+  // ウェブビュー
+  Browser,
+
+
+} = Plugins;
+
+
+/**
+ * URL
+ */
+const url = 'https://takarakuji.rakuten.co.jp/backnumber/';
+
 
 
 interface ItemProps extends RouteComponentProps<{ tab: string }> {
@@ -58,6 +81,39 @@ const MillionsPage: React.FC<MillionsProps> = ({ history, match }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <h2>結果</h2>
+        <IonList>
+          <IonItem onClick={ async () => {
+              console.log('click loto6');
+              /**
+               * ロト6
+               * 遷移
+               */
+              await Browser.open({ url: `${url}loto6/` });
+          }}>
+            <IonLabel>ロト6</IonLabel>
+          </IonItem>
+          <IonItem onClick={ async () => {
+              console.log('click numbers4');
+              /**
+               * ナンバーズ4
+               * 遷移
+               */
+              await Browser.open({ url: `${url}numbers4/`})
+          }}>
+            <IonLabel>ナンバーズ4</IonLabel>
+          </IonItem>
+          <IonItem onClick={ async () => {
+              console.log('click numbers3');
+              /**
+               * ナンバーズ3
+               * 遷移
+               */
+              await Browser.open({ url: `${url}numbers3/`})
+          }}>
+            <IonLabel>ナンバーズ3</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
