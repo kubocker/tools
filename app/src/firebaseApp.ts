bd1fa9bf74a9ef06e786bfdaaeda55b50340f78e
kubocker <kubocker@gmail.com>
2020-08-23 09:40:07 +0900

    Update firebase

--- app/src/firebaseApp.ts
+++ app/src/firebaseApp.ts
@@ -1,15 +1,35 @@
 /* firebase */
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/analytics';
 import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/analytics';
 import 'firebase/storage';
 
 /* app */
 import firebaseConfig from './firebaseConfig';
 
-
+/**
+ * Firebase App
+ */
 export const FirebaseApp = firebase.initializeApp(firebaseConfig);
 
+/**
+ * Firestore
+ */
+export const master = firebase.firestore();
+
+/**
+ * Firebase Realtime Database
+ */
+export const db = firebase.database();
+
+/**
+ * FireStore
+ */
 export const FirebaseStorage = firebase.storage();
+
+/**
+ * Firebase Analytics
+ */
 export const FirebaseAnalytics = firebase.analytics();
